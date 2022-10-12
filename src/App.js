58a50b6e6101da45d779ac7580808ec5effcf836
Rafael Reyes <rafaelreyes@Rafaels-Air.askey.com>
2022-10-12 13:47:57 -0400

    Calendar with default CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import  { useState } from 'react'
 import './App.css';
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
+
+/**
+calendar-date-picker-react
+https://www.youtube.com/watch?v=eM8n55lad1c&t=10s
+https://github.com/wojtekmaj/react-calendar
+**/
 
 function App() {
+
+  const [date, setDate] = useState(new Date())
+  const onChange = date => {    
+    setDate(date)
+  }
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>React Calendar</h1>
+     <div className="calendar">
+      <Calendar showWeekNumbers onChange={onChange} value={date} />
+      {console.log(date)}
+      {date.toString()}
+	</div>
+
     </div>
   );
 }
